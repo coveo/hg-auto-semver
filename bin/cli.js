afcc2de28f7a4713521a36736f2bf1987e9981ce
Germain Bergeron <germain.bergeron1@gmail.com>
2017-04-24 10:57:21 -0400

    Handle breaking changes
    Handle breaking changes to bump major versions
    Update README

--- bin/cli.js
+++ bin/cli.js
@@ -19,13 +19,17 @@ function bump(type) {
 
 try {
     const branch = getParentBranch();
-    const featureRegex = new RegExp('^feature\\-|[^a-z]feature\\-', 'gim');
+    const breakingFeatureRegex = /^breaking-feature-/igm;
+    const featureRegex = /^feature-/igm;
 
-    if (branch && featureRegex.test(branch)) {
+    if (branch && breakingFeatureRegex.test(branch)) {
+        console.log('Branch name contains "breaking-feature-", bumping a MAJOR version');
+        bump(Version.MAJOR);
+    } else if (branch && featureRegex.test(branch)) {
         console.log('Branch name contains "feature-", bumping a MINOR version');
         bump(Version.MINOR);
     } else {
-        console.log('Branch name doesn\'t contains "feature-", bumping a PATCH version');
+        console.log('Branch name doesn\'t contains "breaking-feature-" or "feature-", bumping a PATCH version');
         bump(Version.PATCH);
     }
 } catch (err) {
